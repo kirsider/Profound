9f421288e4bd8fbfe1ba0c3aab51534ba82543f0
kirsider <kirill.sidorenko.2013@mail.ru>
2020-10-29 15:09:01 +0300

    initial commit

---
+++ backend/Profound/Controllers/TestController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Profound.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<char> GetString(string queryString)
+        {
+            queryString ??= "Hello, World!";
+            foreach (char ch in queryString)
+            {
+                yield return ch;
+            }
+        }
+    }
+}
